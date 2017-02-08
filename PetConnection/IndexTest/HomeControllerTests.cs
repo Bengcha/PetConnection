e05a3f5d5723e1f4f29e2e4e3079e9545a85eeed
Kou Cha <Kou Cha>
2017-02-08 17:35:58 -0600

    adding unit tests

---
+++ PetConnection/IndexTest/HomeControllerTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using PetConnection;
+using PetConnection.Controllers;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+
+            //Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void About()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+
+            //Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Your application description page.", result.ViewBag.message);
+        }
+        [TestMethod]
+        public void Contacts()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+
+            //Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void Blog()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+
+            //Act
+            ViewResult result = controller.Blog() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Your contact page.", result.ViewBag.message);
+        }
+
+    }
+}
