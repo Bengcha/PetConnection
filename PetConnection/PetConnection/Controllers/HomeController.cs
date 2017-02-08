8931c0157d6ac686de110812d86d22549f12a2b8
Beng Cha <bengcha04@gmail.com>
2017-02-08 16:37:37 -0600

    added css style, added pcitures, added new nav tab

--- PetConnection/PetConnection/Controllers/HomeController.cs
+++ PetConnection/PetConnection/Controllers/HomeController.cs
@@ -26,5 +26,12 @@ namespace PetConnection.Controllers
 
             return View();
         }
+
+        public ActionResult Blog()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
     }
 }
---
+++ PetConnection/PetConnection/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PetConnection.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public new ActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
